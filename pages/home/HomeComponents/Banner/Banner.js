ff8c34132daeeb44d89f7f060b64fc06f2f9ed86
dariuszb <dariusz.berer@gmail.com>
2021-01-09 09:58:11 +0100

    banner separator

--- pages/home/HomeComponents/Banner/Banner.js
+++ pages/home/HomeComponents/Banner/Banner.js
@@ -1,10 +1,13 @@
 import React from "react";
 const Banner = () => {
   return (
-    <section className="banner">
-      <h1 className="banner__quote">DO GŁOWY POMYSŁY WPADAJĄ OD WEWNĄTRZ</h1>
-      <span className="banner__author">Stanisław Jerzy Lec</span>
-    </section>
+    <React.Fragment>
+      <hr className="separator" />
+      <section className="banner">
+        <h1 className="banner__quote">DO GŁOWY POMYSŁY WPADAJĄ OD WEWNĄTRZ</h1>
+        <span className="banner__author">Stanisław Jerzy Lec</span>
+      </section>
+    </React.Fragment>
   );
 };
 
