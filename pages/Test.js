2fd9732e2337618456dea3701722ee3da75bf569
dariuszb <dariusz.berer@gmail.com>
2021-01-19 20:53:08 +0100

    refactor

--- pages/Test.js
+++ pages/Test.js
@@ -1,28 +1,27 @@
-// import { gql, useMutation } from "@apollo/client";
+import { gql, useMutation } from "@apollo/client";
 import React, { useEffect, useState } from "react";
 
-// const ADD_TODO = gql`
-//   mutation MyMutation {
-//     __typename
-//     sendEmail(
-//       input: {
-//         body: "asdsadsasss"
-//         from: "db@youngmedia.pl"
-//         subject: "AAAAA"
-//         to: "greedo904@gmail.com"
-//       }
-//     ) {
-//       message
-//       origin
-//     }
-//   }
-// `;
+const ADD_TODO = gql`
+  mutation MyMutation {
+    __typename
+    sendEmail(
+      input: {
+        body: "asdsadsasss"
+        from: "db@youngmedia.pl"
+        subject: "AAAAA"
+        to: "greedo904@gmail.com"
+      }
+    ) {
+      message
+      origin
+    }
+  }
+`;
 
 export default function AddTodo() {
-  let input;
-  //   const [addTodo, { data }] = useMutation(ADD_TODO);
-  //   useEffect(() => {
-  //     addTodo("dupa");
-  //   }, []);
+  const [addTodo, { data }] = useMutation(ADD_TODO);
+  useEffect(() => {
+    addTodo("dupa");
+  }, []);
   return <div></div>;
 }
