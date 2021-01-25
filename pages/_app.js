2b0777696f568802feae4aa6e13bdd4a1a360dd7
dariuszb <dariusz.berer@gmail.com>
2021-01-25 21:11:11 +0100

    form

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import "../styles/globals.css";
 import "../styles/HeaderStyle.css";
 import "../styles/FooterStyle.css";
 import "../styles/HomepageStyle.css";
+import "../styles/StronyStyle.css";
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
--- pages/strony-internetowe/index.js
+++ pages/strony-internetowe/index.js
@@ -25,13 +25,15 @@ const UPDATE_TODO = gql`
 export default function StronyInternetowe({
   allPosts: { menus, logos, offers, testimonials },
 }) {
-  const [subject, subjectSet] = useState("test");
+  const [subject, subjectSet] = useState("");
+  const [contact, contactSet] = useState("");
+  const [invalid, invalidSet] = useState(false);
 
   const [updateTodo, { data }] = useMutation(UPDATE_TODO, { client: client });
   let input = {
-    body: "aaassdfsdfaa",
+    body: contact + ":" + subject,
     from: "db@youngmedia.pl",
-    subject: subject,
+    subject: "Strony internetowe",
     to: "greedo904@gmail.com",
   };
 
@@ -50,10 +52,32 @@ export default function StronyInternetowe({
             updateTodo({ variables: { input: input } });
           }}
         >
-          <input onChange={(e) => subjectSet(e.target.value)} />
-          <button type="submit">Add Todo</button>
+          <input
+            onChange={(e) => contactSet(e.target.value)}
+            className={`form__input${!contact && invalid ? "--invalid" : ""}`}
+            placeholder="Email lub telefon"
+          />
+          <textarea
+            onChange={(e) => subjectSet(e.target.value)}
+            className={`form__textarea${
+              !subject && invalid ? "--invalid" : ""
+            }`}
+            placeholder="Wiadomość"
+          />
+          <button
+            type="submit"
+            className={`form__submit--${
+              contact && subject ? "active" : "inactive"
+            }`}
+            onClick={() => {
+              contact && subject
+                ? (e) => subjectSet(e.target.value)
+                : invalidSet(true);
+            }}
+          >
+            Wyślij
+          </button>
         </form>
-        r
       </main>
     </ApolloProvider>
   );
