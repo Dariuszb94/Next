e5ddb71f31f76077bb75c3078ad5856c124bed51
dariuszb <dariusz.berer@gmail.com>
2021-02-16 21:35:06 +0100

    kontakt telefon

--- pages/kontakt/kontaktComponents/icons.js
+++ pages/kontakt/kontaktComponents/icons.js
@@ -1,13 +1,23 @@
 import React, { useEffect, useState } from "react";
 import LocationOnIcon from "@material-ui/icons/LocationOn";
+import Phone from "@material-ui/icons/phone";
+
 export default function Icons() {
   return (
     <section className="icons">
-      <div>
-        <LocationOnIcon className="icons__icon" />
-        <div>
-          PL. BACZYŃSKIEGO 2<br />
-          43-100 TYCHY
+      <div className="icons__flex">
+        <div className="icons__column">
+          <LocationOnIcon className="icons__icon" />
+          <div className="icons__text">
+            PL. BACZYŃSKIEGO 2<br />
+            43-100 TYCHY
+          </div>
+        </div>
+        <div className="icons__column">
+          <Phone className="icons__icon" />
+          <div className="icons__text">
+            <a href="+48502678564">502 678 564</a>
+          </div>
         </div>
       </div>
     </section>
