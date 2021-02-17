57afd32c6de912fe229e97f817be25a8acf1287b
dariuszb <dariusz.berer@gmail.com>
2021-02-17 19:49:34 +0100

    menu link delete prefix

--- pages/header/HeaderComponents/Menu.js
+++ pages/header/HeaderComponents/Menu.js
@@ -7,10 +7,7 @@ const Menu = ({ menuItems, showMenu }) => {
         ? menuItems.edges[0].node.menuItems.edges.map(function (item, i) {
             return (
               <li key={i} className="menu__element">
-                <a
-                  className="menu__element__link"
-                  href={`/Youngmedia${item.node.path}`}
-                >
+                <a className="menu__element__link" href={`/${item.node.path}`}>
                   {item.node.label}
                 </a>
               </li>
